efd48f0bb57922bb3e1d8c6ec2ef9c2d3602522e
kessa99 <kessadavidkipre@gmail.com>
2024-04-17 17:28:00 +0000

    updater request validation

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -14,8 +14,8 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-auth_type = getenv("AUTH_TYPE", 'auth')
-if auth_type == 'auth':
+AUTH_TYPE = getenv("AUTH_TYPE")
+if AUTH_TYPE == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
 
