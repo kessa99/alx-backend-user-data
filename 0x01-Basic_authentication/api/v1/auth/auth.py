5cbc21d8c0812f34b9bf36b93d9c111c01a98905
kessa99 <kipre325@gmail.com>
2024-04-17 16:08:58 +0000

    routes don't need authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -22,6 +22,7 @@ class Auth:
             return None
         return request.headers['Authorization']
 
+
     def current_user(self, request=None) -> TypeVar('User'):
         """Public method to return the current user"""
         return None
@@ -36,8 +37,19 @@ class Auth:
             return False
         return True
     
+
     def authorization_header(self, request=None) -> str:
         """Public method to authorize the header"""
         if request is None or 'Authorization' not in request.headers:
             return None
         return request.headers['Authorization']
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Public method to require authentication"""
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        if path in excluded_paths:
+            return False
+        return True
