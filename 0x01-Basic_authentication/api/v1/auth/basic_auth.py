4de6f42ad094b90452ee3de440991f50fcaf86a0
kessa99 <kessadavidkipre@gmail.com>
2024-04-19 18:51:09 +0000

    update pycodestyle

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,6 +6,7 @@ from flask import jsonify, abort, request
 from typing import List, TypeVar
 from api.v1.auth.auth import Auth
 
+
 class BasicAuth(Auth):
     """BasicAuth class to manage the API authentication"""
     pass
