ab560fabd7e5159693134cb9db3ff8d796ab08e5
kessa99 <kipre325@gmail.com>
2024-04-27 08:52:40 +0000

    add

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,7 +3,7 @@
 Route module for the API
 """
 import logging
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 # logging.disable(logging.WARNING)
 
@@ -34,6 +34,21 @@ def users():
         return jsonify({"email": email, "message": "user created"}), 200
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+    @app.route('/sessions', methods=['POST'], strict_slashes=False)
+    def login() -> str:
+        """POST /sessions
+        Return:
+          - 401: if email or password is incorrect
+          - 200: if login is successful
+        """
+        email, password = request.form.get('email'), request.form.get('password')
+        if not AUTH.valid_login(email, password):
+            abort(401)
+        session_id = AUTH.create_session(email)
+        result = jsonify({"email": email, "message": "logged in"})
+        result.set_cookie("session_id", session_id)
+        return result
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
