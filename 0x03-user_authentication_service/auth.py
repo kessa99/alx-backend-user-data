6b378b3975165ebcd59630f05de186f212432645
kessa99 <kessadavidkipre@gmail.com>
2024-04-27 09:48:40 +0000

    update

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -13,16 +13,19 @@ from uuid import uuid4
 
 logging.disable(logging.WARNING)
 
+
 def _hash_password(password: str) -> str:
     salt = bcrypt.gensalt()
     hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed
+
 
 def _generate_uuid() -> str:
     """
     Generate a UUID
     """
     return str(uuid4())
+
 
 class Auth:
     """
@@ -44,23 +47,18 @@ class Auth:
         except NoResultFound:
             pass
             # print(existing_email)
-            #verifier si l'email exist
+            # verifier si l'email exist
             # if existing_email:
             #     raise ValueError("User {} already exists".format(email))
 
             # hasher le mot de passe si l'email n'existe pas
             hashed_password = _hash_password(password)
 
-            #enregistrer le nouvel user a la base de donne
+            # enregistrer le nouvel user a la base de donne
             user = self._db.add_user(email, hashed_password)
 
             return user
-        # except NoResultFound as e:
-        #     return {"message": str(e)}, 400
 
-        # except ValueError:
-        #     return None
-    
     def find_user_by(self, **kwargs) -> User:
         """
         Find user by a specific attribute
@@ -71,7 +69,7 @@ class Auth:
         except InvalidRequestError:
             return None
 
-    def valid_login(self, email:str, password: str) -> bool:
+    def valid_login(self, email: str, password: str) -> bool:
         """
         validation login
         """
@@ -82,12 +80,11 @@ class Auth:
                 hased_password = user.hashed_password
                 if bcrypt.checkpw(password, user.hashed_password):
                     return True
-        
+
         except NoResultFound:
             return False
         return False
-        
-    
+
     def create_session(self, email: str) -> str:
         """
         Create a new session
@@ -101,10 +98,3 @@ class Auth:
             return {"message": "User {} does not exist".format(email)}, 400
         except NoResultFound:
             return None
-
-    # def is_valid(self, hashed_password: str, password: str) -> bool:
-    #     """
-    #     Validate password
-    #     """
-    #     return bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8'))
-
