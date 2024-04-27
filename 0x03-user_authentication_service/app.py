a623be0a32101b55b1f2bd8fd239c57272957353
kessa99 <kipre325@gmail.com>
2024-04-27 01:11:23 +0000

    add

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,9 +2,20 @@
 """
 Route module for the API
 """
-
+import logging
+from flask import Flask, jsonify
 from views import app
+from auth import Auth
+logging.disable(logging.WARNING)
 
+auth = Auth()
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """
+    WELCOME
+    """
+    return jsonify({"message": "Bienvenue"})
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
