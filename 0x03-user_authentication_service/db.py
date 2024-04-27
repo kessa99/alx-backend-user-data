0dc3e4672f4750f3af3dd585ab76ae198f4aa87a
kessa99 <kipre325@gmail.com>
2024-04-27 00:48:51 +0000

    add

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -60,14 +60,18 @@ class DB:
     def update_user(self, user_id: int, **kwargs) -> None:
         """Update a user
         """
-        user = self.find_user_by(id=user_id)
         try:
-            if user is None:
-                raise NoResultFound
-            else:
-                for key, value in kwargs.items():
-                    setattr(user, key, value)
-
-                self._session.commit()
+            user = self.find_user_by(id=user_id)
+        except NoResultFound:
+            raise NoResultFound("No result found")
+            
+            
+        for key, value in kwargs.items():
+            if not hasattr(user, key):
+                raise ValueError("User haf no attribute {}".format(key))
+            setattr(user, key, value)
+        
+        try:
+            self._session.commit()
         except InvalidRequestError:
             raise
